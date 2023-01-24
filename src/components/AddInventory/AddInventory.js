1bee95295220e31c2453c5d27359eaa44800b170
Darnell Eudoxie <darnelleudoxie@gmail.com>
2023-01-24 15:12:17 -0400

    some css done

--- src/components/AddInventory/AddInventory.js
+++ src/components/AddInventory/AddInventory.js
@@ -1,73 +1,78 @@
 import React from "react";
+import "../AddInventory/AddInventory.scss";
 
 function AddInventory() {
   return (
-    <form className="AddInventory-form">
-      <div className="AddInventory-form__top-box">
-        <h1>Add New Inventory Item</h1>
-        <hr className="form-divider" />
-        <h2>Item Details</h2>
-        <label className="AddInventory-form__label">Item Name</label>
-        <br />
-        <input
-          className="AddInventory-form__item-name-input"
-          type="text"
-          name="item-name"
-          placeholder="Item Name"
-          required
-        ></input>
-        <br />
-        <label className="AddInventory-form__label">Description</label>
-        <br />
-        <input
-          className="AddInventory-form__description-input"
-          type="textarea"
-          name="description"
-          placeholder="Description"
-          required
-        ></input>
-        <br />
-        <label className="AddInventory-form__label">Category</label>
-        <br />
-        <select
-          placeholder="Please Select"
-          className="AddInventory-form__category-dropdown"
-        >
-          <option value="category">category</option>
-        </select>
-        <br />
-        <hr />
-        <h2>Item Availability</h2>
-        <label className="AddInventory-form__label">Status</label>
-        <br />
-        <label className="intock">
-          <input type="radio" name="InStock"></input>In Stock
-        </label>
-        <label className="intock">
-          <input type="radio" name="InStock"></input>Out Of Stock
-        </label>
-        <br />
-        <label className="AddInventory-form__label">Quantity</label>
-        <br />
-        <input
-          className="AddInventory-form__quantity-input"
-          type="text"
-          name="quantity"
-          required
-        ></input>
-        <br />
-        <select
-          placeholder="Please Select"
-          className="AddInventory-form__warehouse-dropdown"
-        >
-          <option value="category">warehouse</option>
-        </select>
-      </div>
-      <div className="AddInventory-form__bottom-box">
-        <button>Cancel</button>
-        <button type="submit">Add Item</button>
-      </div>
-    </form>
+    <div className="AddInventory-box">
+      <form className="AddInventory-form">
+        <div className="AddInventory-form__top-box">
+          <h1 className="AddInventory-form__header">Add New Inventory Item</h1>
+          <hr className="form-divider" />
+          <h2 className="AddInventory-form__sub-titles">Item Details</h2>
+          <label className="AddInventory-form__label-titles">Item Name</label>
+          <br />
+          <input
+            className="AddInventory-form__item-name-input"
+            type="text"
+            name="item-name"
+            placeholder="Item Name"
+            required
+          ></input>
+          <br />
+          <label className="AddInventory-form__label-titles">Description</label>
+          <br />
+          <input
+            className="AddInventory-form__description-input"
+            type="textarea"
+            name="description"
+            placeholder="Description"
+            required
+          ></input>
+          <br />
+          <label className="AddInventory-form__label-titles">Category</label>
+          <br />
+          <select
+            placeholder="Please Select"
+            className="AddInventory-form__category-dropdown"
+          >
+            <option value="category">category</option>
+          </select>
+          <br />
+          <hr />
+          <h2 className="AddInventory-form__sub-titles">Item Availability</h2>
+          <label className="AddInventory-form__label-titles">Status</label>
+          <br />
+          <label className="intock">
+            <input type="radio" name="InStock"></input>In Stock
+          </label>
+          <label className="intock">
+            <input type="radio" name="InStock"></input>Out Of Stock
+          </label>
+          <br />
+          <label className="AddInventory-form__label-titles">Quantity</label>
+          <br />
+          <input
+            className="AddInventory-form__quantity-input"
+            type="text"
+            name="quantity"
+            required
+          ></input>
+          <br />
+          <label className="AddInventory-form__label-titles">Warehouse</label>
+          <br />
+          <select
+            placeholder="Please Select"
+            className="AddInventory-form__warehouse-dropdown"
+          >
+            <option value="category">warehouse</option>
+          </select>
+        </div>
+        <div className="AddInventory-form__bottom-box">
+          <button>Cancel</button>
+          <button type="submit">Add Item</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
