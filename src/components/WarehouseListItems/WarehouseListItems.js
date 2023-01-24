2dd241e566ad441e935b313ce342619e322dd619
Kimberly Nolsin <kimberly.nolsin@gmail.com>
2023-01-24 18:41:59 -0500

    added styling for warehouse list

--- src/components/WarehouseListItems/WarehouseListItems.js
+++ src/components/WarehouseListItems/WarehouseListItems.js
@@ -1,3 +1,6 @@
+import "./WarehouseListItems.scss";
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
 const WarehouseListItems = () => {
   const list = [
     {
@@ -53,25 +56,39 @@ const WarehouseListItems = () => {
 
   const mappedList = list.map((element) => {
     return (
-      <div>
-        <div>
-          <h4>WAREHOUSE</h4>
-          <p>{element.warehouse}</p>
+      <>
+        <div className="warehouse">
+          <div className="warehouse__container">
+            <div>
+              <h4>WAREHOUSE</h4>
+              <p className="warehouse__location">{element.warehouse + '>'}</p>
+            </div>
+            <div>
+              <h4>ADDRESS</h4>
+              <p>{element.address}</p>
+            </div>
+          </div>
+          <div className="warehouse__container">
+            <div>
+              <h4>CONTACT NAME</h4>
+              <p> {element.name}</p>
+            </div>
+            <div>
+              <h4>CONTACT INFORMATION</h4>
+              <p>{element.phone_number}</p>
+              <p>{element.email}</p>
+            </div>
+          </div>
         </div>
-        <div>
-          <h4>ADDRESS</h4>
-          <p>{element.address}</p>
+        <div className="warehouse__icons">
+          <img
+            className="warehouse__delete"
+            src={deleteIcon}
+            alt="delete"
+          ></img>
+          <img className="warehouse__edit" src={editIcon} alt="edit"></img>
         </div>
-        <div>
-          <h4>CONTACT NAME</h4>
-          <p> {element.name}</p>
-        </div>
-        <div>
-          <h4>CONTACT INFORMATION</h4>
-          <p>{element.phone_number}</p>
-          <p>{element.email}</p>
-        </div>
-      </div>
+      </>
     );
   });
 
