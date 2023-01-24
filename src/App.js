9c794f513ca6cd95c493434f2c68026f87439788
Darnell Eudoxie <darnelleudoxie@gmail.com>
2023-01-24 11:34:37 -0400

    starting skeleton

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Warehouse from "./pages/Warehouse";
 import Warehouse_details from "./components/WareHouse_details/Warehouse_details";
 import Inventory from "./pages/Inventory";
 import "./App.css";
+import AddInventory from "./components/AddInventory/AddInventory";
 
 function App() {
   return (
     <BrowserRouter>
+      <AddInventory />
       <Routes>
         <Route path="/warehouse" element={<Warehouse />} />
         <Route path="/warehouse_list" element={<Warehouse_details />} />
--- src/components/AddInventory/AddInventory.js
+++ src/components/AddInventory/AddInventory.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function AddInventory() {
-  return <div>AddInventory</div>;
+  return (
+    <form className="AddInventory-form">
+      <div className="AddInventory-form__top-box">
+        <h1>Add New Inventory Item</h1>
+        <hr className="form-divider" />
+        <label className="AddInventory-form__input-label">Item Name</label>
+        <input
+          className="AddInventory-form__input"
+          type="text"
+          name="item-name"
+          placeholder="Item Name"
+          required
+        ></input>
+      </div>
+    </form>
+  );
 }
 
 export default AddInventory;
