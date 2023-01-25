a516bfd17a983b085afe0e609dfc24be98df6b8e
Nick Abate <nick.abate@outlook.com>
2023-01-24 23:39:27 -0500

    Working component to be styled further.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Warehouse from "./pages/Warehouse";
 import Warehouse_details from "./components/WareHouse_details/Warehouse_details";
 import Inventory from "./pages/Inventory";
+import WarehouseInventoryList from "./components/WarehouseInventoryList/WarehouseInventoryList";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,10 @@ function App() {
         <Route path="/warehouse" element={<Warehouse />} />
         <Route path="/warehouse_list" element={<Warehouse_details />} />
         <Route path="/inventory" element={<Inventory />} />
+        <Route
+          path="/warehouseinventorylist"
+          element={<WarehouseInventoryList />}
+        />
       </Routes>
     </BrowserRouter>
   );
