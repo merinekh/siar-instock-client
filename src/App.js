4fb4f3b8a4c13026fb5e162558b2ecaf90c1d624
Anvar Gulomov <anvar.gulomov.97@gmail.com>
2023-01-25 04:28:59 -0500

    page header add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Warehouse from "./pages/Warehouse";
-import Warehouse_details from "./components/WareHouse_details/Warehouse_details";
 import Inventory from "./pages/Inventory";
-import "./App.css";
+import Layout from "./pages/Layout";
+import { AppRoute } from "./const";
+import './styles/style.scss'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/warehouse" element={<Warehouse />} />
-        <Route path="/warehouse_list" element={<Warehouse_details />} />
-        <Route path="/inventory" element={<Inventory />} />
+      <Route path={AppRoute.HOME} element={<Layout />}>
+        <Route path={AppRoute.WAREHOUSE} element={<Warehouse />} />
+        <Route path={AppRoute.INVENTORY} element={<Inventory />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
