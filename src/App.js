aaafbb2baec3ac1929534d692736d2c0960b2d5c
Darnell Eudoxie <darnelleudoxie@gmail.com>
2023-01-26 21:36:08 -0400

    files to push

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Inventory from "./pages/Inventory/Inventory";
 import Layout from "./pages/Layout";
 import { AppRoute } from "./const";
 import "./styles/style.scss";
-//import Warehouse from "./pages/Warehouse/Warehouse";
+import Warehouse from "./pages/Warehouse/Warehouse";
 import EditAddWarehouseForm from "./components/EditAddWarehouseForm/EditAddWarehouseForm";
+import AddInventory from "./components/AddInventory/AddInventory";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={AppRoute.HOME} element={<Layout />}>
-          {/* <Route path={AppRoute.WAREHOUSE} element={<Warehouse />} /> */}
+          <Route path={AppRoute.WAREHOUSE} element={<Warehouse />} />
+          <Route path={AppRoute.ADDINVENTORY} element={<AddInventory />} />
           <Route
             path={AppRoute.EDIT_WAREHOUSE}
             element={<EditAddWarehouseForm />}
